6b5705ce6a08914ea965435ce4962c34fe720565
kevin_2218131@hotmail.com <kevin_2218131@hotmail.com>
2020-04-09 03:46:35 +0100

    Added create tags feature

---
+++ app/recipe/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from core.models import Tag
+from django.utils.translation import  ugettext_lazy as _
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id','name')
+        read_only_fields = ('id',)
+        
