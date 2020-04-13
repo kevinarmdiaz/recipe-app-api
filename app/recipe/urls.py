2b86644319de5e640fc0aa4996d70b7a195c0bb7
kevin_2218131@hotmail.com <kevin_2218131@hotmail.com>
2020-04-13 04:01:30 +0100

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,8 @@ router = DefaultRouter()
 
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
+
 
 app_name = 'recipe'
 
